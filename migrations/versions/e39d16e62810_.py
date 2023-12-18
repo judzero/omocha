8a448e0d4960996d64cb8366940f1721b51f734f
James Francies de Lima Gamis <hardwellapollo1029@gmail.com>
2023-12-18 21:03:45 +0800

    fixed forget password
    fixed forget password

---
+++ migrations/versions/e39d16e62810_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: e39d16e62810
+Revises: 
+Create Date: 2023-12-18 20:41:05.540108
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'e39d16e62810'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
